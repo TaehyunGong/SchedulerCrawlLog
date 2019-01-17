f643b47afa437be5aaa8a157c2a09882efd07521
TaehyunGong <38848719+TaehyunGong@users.noreply.github.com>
2019-01-18 00:21:22 +0900

    hi

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from CrwalingMulitProcessing import CrwalingMulitProcessing
 import time
 import TestFile
-from TestFile import createNouns
 
 #2019-01-15
 if __name__ == '__main__' :
 
     startTime = time.time()
-    newNouns = createNouns()
 
-    CrwalingMulitProcessing().startMain(newNouns)
-
-    nobj = TestFile.createNouns()
+    CrwalingMulitProcessing().startMain()
 
     print('경과시간 : ', time.time() - startTime)
